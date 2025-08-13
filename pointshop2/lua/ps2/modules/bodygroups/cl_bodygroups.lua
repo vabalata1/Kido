d9f923eb6649333eca9be4aaa78eb374a9e9bbc6
vaba <92249829+vabalata1@users.noreply.github.com>
2025-08-13 15:20:45 +0100

    Add files via upload

---
+++ pointshop2/lua/ps2/modules/bodygroups/cl_bodygroups.lua
@@ -0,0 +1,48 @@
+local Player = LocalPlayer()
+
+Player.BodygroupsData = {}
+
+function MakeNiceName( str )
+  local newname = {}
+
+  for _, s in pairs( string.Explode( "_", str ) ) do
+    if ( string.len( s ) == 1 ) then table.insert( newname, string.upper( s ) ) continue end
+    table.insert( newname, string.upper( string.Left( s, 1 ) ) .. string.Right( s, string.len( s ) - 1 ) )
+  end
+
+  return string.Implode( " ", newname )
+end
+
+hook.Add( "PS2_GetPreviewModel", "BodygroupSupport", function( )
+  if Pointshop2:IsPlayerModelEquipped() then
+    local playerModelItem = Player.PS2_Slots["Model"]
+    local BodyGroups = Player.BodygroupsData[playerModelItem.id]
+
+    return {
+      model = playerModelItem.playerModel,
+      skin = BodyGroups and BodyGroups[1] or 0,
+      bodygroups = BodyGroups and BodyGroups[2] or "0"
+    }
+  end
+
+  return {
+    model = Player:GetModel(),
+    bodygroups = "0",
+    skin = 0
+  }
+end)
+
+net.Receive("Bodygroups_Init", function( len )
+  local id = net.ReadUInt(16)
+  local skin = net.ReadUInt(5)
+  local groups = net.ReadString()
+
+  Player.BodygroupsData[id] = {skin, string.gsub(groups, ".", " %0"):sub(2)}
+
+  hook.Run("PS2_DoUpdatePreviewModel")
+end)
+
+hook.Add( "PS2_PopulateCredits", "AddBodygroupCreds", function( panel )
+  panel:AddCreditSection("[Kido] F3 Bodygroup", [[ by zeeniik ]])
+  --c'est absolument faux mais j'vous baise (ou pas <3)
+end)
---
+++ pointshop2/lua/ps2/modules/bodygroups/sh_module.lua
@@ -0,0 +1,15 @@
+local MODULE = {}
+
+MODULE.Name = "Bodygroup"
+MODULE.Author = "zeeniik"
+
+MODULE.Blueprints = {}
+
+MODULE.SettingButtons = {}
+
+MODULE.Settings = {
+  Shared = {},
+  Server = {}
+}
+
+Pointshop2.RegisterModule( MODULE )
